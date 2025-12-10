c8c8fd46640a17d5c8569d16478f3bc8e74a2ec4
trideepshivam <trideepshivam@gmail.com>
2025-12-10 18:48:58 +0530

    created book and candidate header with the declaration of defined function along with the file C

---
+++ Project/LibraryManagementSystem/Book/book.h
@@ -0,0 +1,30 @@
+/*
+ * book.h
+ * Header collecting Book structure and function declarations
+ */
+
+#ifndef BOOK_H
+#define BOOK_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct {
+    char B_name[100];
+    char Author[50];
+    int price;
+    char Genere[50];
+    int B_count;
+    int Book_ID;
+} Book;
+
+/* Function prototypes from book.c */
+void B_Register(Book B_R);
+Book B_Retrive(int id);
+void get_book_by_author(const char *author);
+int get_ID(void);
+void get_book_by_genere(const char *genere);
+void get_book_by_name(const char *name);
+
+#endif /* BOOK_H */
--- Project/LibraryManagementSystem/LiveReading/LiveReading.c
+++ Project/LibraryManagementSystem/LiveReading/LiveReading.c
@@ -29,10 +29,21 @@ void initiate(LiveReading lr){
     fclose(f);
     printf("Data written successfully!\n");
 }
-
+void showLiveRreading(){
+    FILE *f;
+    LiveReading lr;
+    f =fopen("LiveReading.dat","rb"); //rb ="Reading in Binory..."
+    if(f==NULL){
+        printf("File not read.");
+    }
+    while(fread(&lr,sizeof(LiveReading),1,f)==1){
+        printf("%d %d %d:%d:%d %d:%d:%d %d/%d/%d\n",lr.C_ID,lr.B_ID,lr.entry.hr,lr.entry.min,lr.entry.sec,lr.exit.hr,lr.exit.min,lr.exit.sec,lr.date.day,lr.date.month,lr.date.year);
+    }
+}
 void main(){
-    LiveReading lr={3,6,{10,30,0},{12,0,0},{15,8,2024}};
-    initiate(lr);
+    // LiveReading lr={3,6,{10,30,0},{12,0,0},{15,8,2024}};
+    // initiate(lr);
+    showLiveRreading();
 }
 
 
