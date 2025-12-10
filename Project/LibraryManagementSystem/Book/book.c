31a7f5e864f584f98816890d06645732de792fdc
Roshan Roy <rr6711608@gmail.com>
2025-12-10 20:21:43 +0530

    added one function Showall_Book ()

--- Project/LibraryManagementSystem/Book/book.c
+++ Project/LibraryManagementSystem/Book/book.c
@@ -144,8 +144,47 @@ void get_book_by_name(const char *name) {
     fclose(f);
 }
 
+//function for update count of book with same name..same author..same genere...
+void update_book_count(int ID ,int count) {
+    Book B_R;
+    FILE *f = fopen("Book.dat", "rb+");  // rb = read binary and write binary
+
+    if (f == NULL) {
+        printf("File doesn't exist.\n");
+    }
+
+    while (fread(&B_R, sizeof(Book), 1, f)) {
+        if(ID == B_R.Book_ID){
+            B_R.B_count+=count;
+            fseek(f,-sizeof(Book),SEEK_CUR);
+            fwrite(&B_R,sizeof(Book),1,f);
+            printf("Book count uptaded successfily.\n");
+            break;
+        }
+    }
+
+    if(fread(&B_R, sizeof(Book), 1, f)==0){
+        printf("ID not found");
+    }
+}
 
 
+//fuction for showall all book...
+Book Showall_Book (){
+    Book B_R;
+    FILE *f;
+    f = fopen("Book.dat","rb");  //rb = "readin into Binory..."
+       if(f==NULL){
+        printf("File doesn't exist.");
+    }
+    printf("BOOK DETAILS :\n---------------------------------------------------------------------------------\n%-5s | %-20s | %-13s | %-15s | %-8s | %-5s\n---------------------------------------------------------------------------------\n","ID","NAME","AUTHOR","GENERE","PRICE","COUNT");
+    while(fread(&B_R, sizeof(Book), 1, f)){
+        printf("%-5d | %-20s | %-13s | %-15s | %-8d | %-5d\n",B_R.Book_ID,B_R.B_name,B_R.Author,B_R.Genere,B_R.price,B_R.B_count);
+       
+    }
+    fclose(f);
+
+}
 
 
 
@@ -156,15 +195,17 @@ int main() {
     // Book B = {"Fundamentals", "Shivam", 343, "Inspiration", 1, 25};
     // B_Register(B);
 
-    char name[50];
-    printf("Enter book name : ");
-    scanf(" %[^\n]", name);
-   get_book_by_name(name);
+    //int ID,count;
+    //printf("Enter book ID and count update value : ");
+    //scanf("%d %d",&ID,&count);
+    //update_book_count(ID,count);
+
+   //get_book_by_name("Programming with C++");
 
     //get_book_by_author(author);
     //int temp =get_ID();
     //printf("%d",temp);
     //return 0;
-
+    Showall_Book ();
 }
 
