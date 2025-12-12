f1f74b60e799a122d555f8860e3942f4a31eb567
Danish Shaikh <danish.mdp2024@gmail.com>
2025-12-12 19:19:08 +0530

    created getTransactionById which will recieve an id and print transaction detail

--- Project/LibraryManagementSystem/Transaction/transaction.c
+++ Project/LibraryManagementSystem/Transaction/transaction.c
@@ -26,10 +26,32 @@ void getAllTransaction(){
     }
     fclose(f);
 }
-
-void main(){
-    // Transaction t = {1,340,{9,50,30},{12,12,2012}};
-    // logTransation(t);
-    getAllTransaction();
+void getTransactionById(int id){
+    Transaction t;
+    FILE *fp;
+    fp=fopen("../data/Transaction.dat","rb");
+    if(fp==NULL){
+         printf("File not found");
+    }
+    while(fread(&t,sizeof(Transaction),1,fp)==1){
+        if(t.id==id){
+            printf("TRANSACTION DETAILS :\n--------------------------------------\n%-5s | %-6s | %-10s | %-12s\n----------------------------------------\n","ID","AMOUNT","TIME","DATE");
+            printf("%-5d | %-6d | %-2d:%-2d:%-2d | %-2d/%-2d/%-4d\n",t.id,t.amount,t.time.hr,t.time.min,t.time.sec,t.date.day,t.date.month,t.date.year);
+            break;
+        }
+    }
+    if(fread(&t,sizeof(Transaction),1,fp)==0){
+        printf("invalid id\n");
+    }
 }
+void getTransactionByDate(Date date){
+    //code
+}
+
+// void main(){
+//     Transaction t = {2,540,{9,10,30},{14,12,2015}};
+//     logTransation(t);
+//     //getAllTransaction();
+//     getTransactionById(2);
+// }
 
