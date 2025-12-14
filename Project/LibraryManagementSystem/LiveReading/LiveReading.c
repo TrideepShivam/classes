0c61bcc01f74600351d3f1edf7bef8a476e2692c
trideepshivam <trideepshivam@gmail.com>
2025-12-14 22:01:20 +0530

    main removed from live reading

--- Project/LibraryManagementSystem/LiveReading/LiveReading.c
+++ Project/LibraryManagementSystem/LiveReading/LiveReading.c
@@ -23,16 +23,16 @@ void showLiveRreading(){
         return;
     }
     while(fread(&lr,sizeof(LiveReading),1,f)==1){
-        Book b = B_Retrive(26);
+        Book b = B_Retrive(1);
         printf("%s\n",b.B_name);
         printf("%d %d %d:%d:%d %d:%d:%d %d/%d/%d\n",lr.C_ID,lr.B_ID,lr.entry.hr,lr.entry.min,lr.entry.sec,lr.exit.hr,lr.exit.min,lr.exit.sec,lr.date.day,lr.date.month,lr.date.year);
     }
 }
-void main(){
-    // LiveReading lr={1,26,{10,30,0},{12,0,0},{15,8,2024}};
-    // initiate(lr);
-    showLiveRreading();
-}
+// void main(){
+//     // LiveReading lr={1,26,{10,30,0},{12,0,0},{15,8,2024}};
+//     // initiate(lr);
+//     showLiveRreading();
+// }
 
 
 
