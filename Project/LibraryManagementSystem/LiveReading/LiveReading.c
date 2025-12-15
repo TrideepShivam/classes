5af575c742ff9917a1a761c0a61a8067a760e2ee
trideepshivam <trideepshivam@gmail.com>
2025-12-15 22:44:25 +0530

    updated data path and added book name in the show details

--- Project/LibraryManagementSystem/LiveReading/LiveReading.c
+++ Project/LibraryManagementSystem/LiveReading/LiveReading.c
@@ -5,7 +5,7 @@
 
 void initiate(LiveReading lr){
     FILE *f;
-    f = fopen("data/LiveReading.dat","ab");  //ab = "Appending in Binory..."
+    f = fopen("../data/LiveReading.dat","ab");  //ab = "Appending in Binory..."
        if(f==NULL){
         printf("File doesn't exist.\n");
         return;
@@ -23,16 +23,15 @@ void showLiveRreading(){
         return;
     }
     while(fread(&lr,sizeof(LiveReading),1,f)==1){
-        Book b = B_Retrive(1);
-        printf("%s\n",b.B_name);
-        printf("%d %d %d:%d:%d %d:%d:%d %d/%d/%d\n",lr.C_ID,lr.B_ID,lr.entry.hr,lr.entry.min,lr.entry.sec,lr.exit.hr,lr.exit.min,lr.exit.sec,lr.date.day,lr.date.month,lr.date.year);
+        Book b = B_Retrive(lr.B_ID);
+        printf("%-4d | %-4d| %-30s| %2d:%2d:%2d| %2d:%2d:%2d| %2d/%2d/%4d\n",lr.C_ID,lr.B_ID,b.B_name,lr.entry.hr,lr.entry.min,lr.entry.sec,lr.exit.hr,lr.exit.min,lr.exit.sec,lr.date.day,lr.date.month,lr.date.year);
     }
 }
-// void main(){
-//     // LiveReading lr={1,26,{10,30,0},{12,0,0},{15,8,2024}};
-//     // initiate(lr);
-//     showLiveRreading();
-// }
+void main(){
+    //LiveReading lr={102,2,{10,00,0},{12,0,0},{15,8,2025}};
+    //initiate(lr);
+    showLiveRreading();
+}
 
 
 
